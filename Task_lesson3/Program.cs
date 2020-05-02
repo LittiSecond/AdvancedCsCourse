b8dc837ebc21e42c153600832c3df18fa78ff403
LittiSecond <breeze2882@yandex.ru>
2020-05-02 09:46:14 +0300

    Выполнено задание 5

--- Task_lesson3/Program.cs
+++ Task_lesson3/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_lesson3
 {
     public delegate void MyDeligate(object o);
+    public delegate void OurDeligate<T>(T arg);
 
     // не хочу в этом примере разносить классы в разные файлы
 
     class Source
     {
         public event MyDeligate Run;
+        public event OurDeligate<string> RunS;
+        public event OurDeligate<int> RunI;
+
+        private int _index;
+        private string _name;
+
+        public Source(int index, string name)
+        {
+            _index = index;
+            _name = name;
+        }
 
         public void Start()
         {
             Console.WriteLine("RUN");
-            if (Run != null) Run(this);
+            Run?.Invoke(this);
+            RunS?.Invoke(_name);
+            RunI?.Invoke(_index);
         }
+
+        public override string ToString() => _name + " с индексом " + _index.ToString();
     }
 
     class Observer1
@@ -37,18 +49,38 @@ namespace Task_lesson3
         }
     }
 
+    class Observer3<T2>
+    {
+        private readonly string _name;
+
+        public Observer3(string name)
+        {
+            _name = name;
+        }
+
+        public void Do(T2 arg)
+        {
+            Console.WriteLine("{1}. Принял, что объект {0} побежал", arg, _name);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Source s = new Source();
+            Source s = new Source(1000, "Тысячный");
             Observer1 o1 = new Observer1();
             Observer2 o2 = new Observer2();
+            Observer3<int> o3 = new Observer3<int>("Третий");
+            Observer3<string> o4 = new Observer3<string>("Четвёртый");
             MyDeligate d1 = new MyDeligate(o1.Do);
             s.Run += d1;
             s.Run += o2.Do;
+            s.RunI += o3.Do;
+            s.RunS += o4.Do;
             s.Start();
             s.Run -= d1;
+            s.RunI -= o3.Do;
             s.Start();
 
             Console.ReadLine();
