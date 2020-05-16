87b8dd3edfc86a5393c4eebf13bd139013c5e320
LittiSecond <breeze2882@yandex.ru>
2020-05-16 11:24:27 +0300

    Скопированы формы из задания к уроку 5

---
+++ Task_lesson7/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_lesson7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Print(string str)
+        {
+            LogTextBox.AppendText(str + "\n");
+        }
+
+        private void LoadMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Print("LoadMenuItem_Click:");
+        }
+
+        private void ClearLogMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            LogTextBox.Clear();
+        }
+
+        private void WorkerAboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            Print("WorkerAboutButton_Click:");
+        }
+    }
+}
