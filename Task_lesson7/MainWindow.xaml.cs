a140c0c7d42306d5abb1bf7fbe34babafb791b52
LittiSecond <breeze2882@yandex.ru>
2020-05-17 23:05:42 +0300

    Сделан только вывод в форму данных из базы данных

--- Task_lesson7/MainWindow.xaml.cs
+++ Task_lesson7/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 
+using Task_lesson7.Entities;
+using Task_lesson7.VievModels;
+
 namespace Task_lesson7
 {
     /// <summary>
@@ -21,9 +24,14 @@ namespace Task_lesson7
     /// </summary>
     public partial class MainWindow : Window
     {
+        private StaffObservable _staff;
+
         public MainWindow()
         {
             InitializeComponent();
+            _staff = this.Resources["Staff"] as StaffObservable;
+            _staff.GetConnectionString = SendConnectionString;
+            _staff.SendMessage = Print;
         }
 
         private void Exit(object sender, RoutedEventArgs e)
@@ -39,6 +47,7 @@ namespace Task_lesson7
         private void LoadMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Print("LoadMenuItem_Click:");
+            _staff.ReloadAll();
         }
 
         private void ClearLogMenuItem_Click(object sender, RoutedEventArgs e)
@@ -49,6 +58,48 @@ namespace Task_lesson7
         private void WorkerAboutButton_Click(object sender, RoutedEventArgs e)
         {
             Print("WorkerAboutButton_Click:");
+
+            //int ind = FullWorkersList.SelectedIndex;
+            //if (ind < 0 || ind > _fullEmployeeInfosList.Count)
+            //    return;
+
+            //EmployeeInfo selectedEmp = _fullEmployeeInfosList[ind];
+
+            AboutEmployeeWindow wind = new AboutEmployeeWindow();
+            wind.Owner = this;
+            wind.DataContext = _staff;
+
+            //wind.EmployeeInfo = selectedEmp;
+            //wind.DepartamentsInfo = _departamentInfoList;
+            //wind.ShowInfo();
+
+            //wind.Show();
+            wind.ShowDialog();
         }
+
+        private string SendConnectionString()
+        {
+            return Task_lesson7.Properties.Settings.Default.StaffConnectionString;
+        }
+
+        private void TestMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            _staff.Test1();
+        }
+
+        private void FullWorkersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int ind = FullWorkersList.SelectedIndex;
+            if (ind < 0)
+            {
+                WorkerAboutButton.IsEnabled = false;
+            }
+            else
+            {
+                WorkerAboutButton.IsEnabled = true;
+            }
+        }
+
+
     }
 }
--- Task_lesson7/VievModels/StaffObservable.cs
+++ Task_lesson7/VievModels/StaffObservable.cs
@@ -5,17 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
+//using System.Runtime.CompilerServices;
 
 using Task_lesson7.Entities;
+
 
 namespace Task_lesson7.VievModels
 {
     class StaffObservable : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        //public delegate string GetConnectionString();
+        public Func<string> GetConnectionString;
+        /// <summary> делегат для отправки текстовых сообщений во внешнюю среду </summary>
+        public Action<string> SendMessage;
+
+        private Employee _selectedEmploye;
+        public Employee SelectedEmployee
+        {
+            get { return _selectedEmploye; }
+            set { _selectedEmploye = value; }
+        }
 
         private ObservableCollection<Employee> _employeesO;
         private ObservableCollection<Departament> _departamentsO;
+
 
         public StaffObservable()
         {
@@ -39,6 +55,147 @@ namespace Task_lesson7.VievModels
         public void Employ(Employee w)
         {
             _employeesO.Add(w);
+        }
+
+
+
+        public void ReloadAll()
+        {
+            _employeesO.Clear();
+            _departamentsO.Clear();
+            if (GetConnectionString == null)
+            {
+                SendMessage?.Invoke("StaffObservable->ReloadAll: Ошибка! Не получить строку подключения.");
+                return;
+            }
+
+            string sqlExpresion = @"SELECT * FROM [Staff].[dbo].[Departaments]";
+
+            List<int> chiefs = new List<int>();
+
+            string con = GetConnectionString();
+
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpresion, connection);
+                //SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32( reader["Id"]);
+                        string name = Convert.ToString(reader["Name"]);
+                        string info = Convert.ToString(reader["Info"]);
+                        object chiefO = reader["Chief"];
+                        //int chiefId = Convert.ToInt32(chiefO);
+                        //int chiefId = (chiefO != null) ? Convert.ToInt32(chiefO) : -1;
+                        int chiefId = (chiefO is DBNull) ? -1 : Convert.ToInt32(chiefO);
+                        chiefs.Add(chiefId);
+                        _departamentsO.Add(new Departament(id, name, info));
+                    }
+                }
+
+            }
+            
+            sqlExpresion = @"SELECT * FROM [Staff].[dbo].[Employees]";
+
+            List<int> depsId = new List<int>();
+
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpresion, connection);
+                //SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            int id = Convert.ToInt32(reader["Id"]);
+                            string firstName = Convert.ToString(reader["FirstName"]);
+                            string surName = Convert.ToString(reader["SurName"]);
+                            string appointment =  Convert.ToString(reader["Appointment"]);                          
+                            string info = Convert.ToString(reader["Info"]);
+                            object depO = reader["DepartamentId"];
+                            int depId = (depO is DBNull) ? -1 : Convert.ToInt32(depO);
+                            depsId.Add(depId);
+                            object salO = reader["Salary"];
+                            double salary = (salO is DBNull) ? 0 : Convert.ToDouble(salO);
+
+                            Employee emp = new Employee(firstName, surName, id)
+                            {
+                                Appointment = appointment,
+                                Info = info,
+                                Salary = salary
+                            };
+
+                            if (depId> 0)
+                            {
+                                Departament dep = _departamentsO.FirstOrDefault(d => d.Id == depId);
+                                dep.AddEmployee(emp);
+                            }
+
+                            _employeesO.Add(emp);
+
+                        }
+                    }
+            }
+            
+            // расстановка вождей по отделам
+            for (int i = 0; i < chiefs.Count; i++)
+            {
+                int id = chiefs[i];
+                if (id > 0)
+                {
+                    Departament dep = _departamentsO[i];
+                    dep.Chief = _employeesO.FirstOrDefault(e => e.Id == id);
+                }
+            }
+
+
+            OnPropertyChanged("DepartamentsO");
+            OnPropertyChanged("EmployeesO");
+
+        }
+
+
+
+
+
+
+
+        private void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        public void Test1()
+        {
+            if (GetConnectionString == null)
+            {
+                SendMessage?.Invoke("StaffObservable->ReloadAll: Ошибка! Не получить строку подключения.");
+                return;
+            }
+
+            string sqlExpresion = @"SELECT COUNT(*) FROM [Staff].[dbo].[Employees]";
+            //string sqlExpresion = @"SELECT COUNT(*) FROM [dbo].[Employees]";
+            //string sqlExpresion = @"SELECT COUNT(*) FROM Employees";
+
+            string con = GetConnectionString();
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(sqlExpresion, connection);
+
+                int c = Convert.ToInt32(command.ExecuteScalar());
+                SendMessage?.Invoke($"StaffObservable->Test1: '{sqlExpresion}' = {c}");
+
+            }
         }
 
 
@@ -83,6 +240,8 @@ namespace Task_lesson7.VievModels
         }
 
         #endregion
+
+
 
     }
 }
