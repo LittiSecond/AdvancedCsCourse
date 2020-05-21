ed57e59af9c0f74e13f9ae783b4c222e7a9d7011
LittiSecond <breeze2882@yandex.ru>
2020-05-21 07:12:58 +0300

    Подготовка клиента (скопирован из предыдущего задания и подчищен)

---
+++ Task_lesson8_client/VievModels/StaffObservable.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
+//using System.Runtime.CompilerServices;
+
+using Task_lesson7.Entities;
+
+
+namespace Task_lesson7.VievModels
+{
+    class StaffObservable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary> делегат для отправки текстовых сообщений во внешнюю среду </summary>
+        public Action<string> SendMessage;
+
+        private Employee _selectedEmploye;
+        public Employee SelectedEmployee
+        {
+            get { return _selectedEmploye; }
+            set { _selectedEmploye = value; }
+        }
+
+        private ObservableCollection<Employee> _employeesO;
+        private ObservableCollection<Departament> _departamentsO;
+
+
+        public StaffObservable()
+        {
+            _employeesO = new ObservableCollection<Employee>();
+            _departamentsO = new ObservableCollection<Departament>();
+
+            StubCreateStaff();
+        }
+
+        public ObservableCollection<Employee> EmployeesO
+        {
+            get { return _employeesO; }
+        }
+
+        public ObservableCollection<Departament> DepartamentsO
+        {
+            get { return _departamentsO; }
+        }
+
+
+        public void Employ(Employee w)
+        {
+            _employeesO.Add(w);
+        }
+
+
+
+        public void ReloadAll()
+        {
+            SendMessage?.Invoke("StaffObservable->ReloadAll: ");
+
+            //_employeesO.Clear();
+            //_departamentsO.Clear();
+
+
+
+            //OnPropertyChanged("DepartamentsO");
+            ///OnPropertyChanged("EmployeesO");
+
+        }
+
+
+
+
+
+
+
+        private void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        public void Test1()
+        {
+            SendMessage?.Invoke("StaffObservable->Test1: ");
+
+        }
+
+
+        #region заглушка
+        /// <summary>
+        /// заглушка, пока не пределаю источник данных
+        /// </summary>
+        private void StubCreateStaff()
+        {
+            Departament dep1 = new Departament(85, "Отдел 1");
+            Departament dep2 = new Departament(87, "Второй Отдел", "начальника не назначать");
+            _departamentsO.Add(dep1);
+            _departamentsO.Add(dep2);
+
+            Employee emp1 = new Employee("Имя1", "surИмя1", 701)
+            {
+                Appointment = "должность1",
+                Salary = 1001,
+                Info = "инфо1"                
+            };
+            Employee emp2 = new Employee("Имя2", "surИмя2", 702)
+            {
+                Appointment = "должность2",
+                Salary = 2002,
+                Info = "инфо2"
+            };
+            Employee emp3 = new Employee("Имя3", "surИмя3", 703)
+            {
+                Appointment = "должность3",
+                Salary = 3003,
+                Info = "инфо3"
+            };
+
+            _employeesO.Add(emp1);
+            _employeesO.Add(emp2);
+            _employeesO.Add(emp3);
+
+            dep1.AddEmployee(emp1);
+            dep1.SetChief(emp1);
+            dep1.AddEmployee(emp2);
+            dep2.AddEmployee(emp3);
+        }
+
+        #endregion
+
+
+
+    }
+}
